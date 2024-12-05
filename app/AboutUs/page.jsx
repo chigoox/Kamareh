8455439da9e648478ded27270b6683a1f39ac6a4
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-05 17:59:11 -0500

    Update page.jsx

---
+++ app/AboutUs/page.jsx
@@ -0,0 +1,12 @@
+import { Button, Card, Image, Input, Link } from "@nextui-org/react";
+
+
+function AboutUs() {
+  return (
+    <div className='bg-white hidescroll  h-screen   relative overflow-x-hidden'>
+     ABOUT US
+    </div>
+  )
+}
+
+export default AboutUs
