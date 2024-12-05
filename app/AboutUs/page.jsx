f6bc3203f86199e9649b21679152dda71b9912db
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-05 18:59:26 -0500

    Update page.jsx

--- app/AboutUs/page.jsx
+++ app/AboutUs/page.jsx
@@ -4,7 +4,7 @@ import { Button, Card, Image, Input, Link } from "@nextui-org/react";
 function AboutUs() {
   return (
     <div className='bg-white hidescroll  h-screen   relative overflow-x-hidden'>
-      <Image className={`w-full h-96 rounded-none`} src={'Images/guy (8).jpg'} alt='picture'/>
+      <Image className={`w-full h-96 rounded-none object-cover`} src={'Images/guy (8).jpg'} alt='picture'/>
     <div className={`center-col w-full md:w-1/3 p-2 m-auto`}>
 
        <div className={``}>
@@ -13,21 +13,21 @@ function AboutUs() {
        The name “Kamareh” is a blend of Mameh’s first and last name, symbolizing the seamless fusion of personal
        identity and creative expression. 
      </div>
-      <Image className={`w-full h-96`} src={'Images/guy (2).jpg'} alt='picture'/>
+      <Image className={`w-full h-96 object-cover`} src={'Images/guy (2).jpg'} alt='picture'/>
       <div className={``}>
        Kamareh was born out of Mameh’s desire to offer a clothing line that transcends traditional boundaries.
         Inspired by her own fluid sense of style, Kamareh is designed for those who refuse to be boxed into one
         category of fashion. Whether you’re drawn to the edge of streetwear or the sophistication of classical styles,
         Kamareh offers a diverse range of pieces that allow you to express yourself freely, without compromise.        
       </div>
-      <Image className={`w-full h-24`} src={'Images/guy (3).jpg'} alt='picture'/>
+      <Image className={`w-full h-24 object-cover`} src={'Images/guy (3).jpg'} alt='picture'/>
       <div className={``}>
         At Kamareh, we believe that clothing is a canvas for personal expression. Our collections are meticulously crafted
         to blend comfort, quality, and versatility, empowering individuals to mix and match, experiment, and create their own
         signature look. We are proud to provide fashion-forward designs that cater to those who embrace the freedom to define
         their own style—whether it’s sleek, casual, bold, or refined. 
       </div>
-<Image className={`w-full h-96`} src={'Images/guy (6).jpg'} alt='picture'/>
+<Image className={`w-full h-96 object-cover`} src={'Images/guy (6).jpg'} alt='picture'/>
       <div className={``}>
         Kamareh is more than just a brand; it’s a movement for those who celebrate the beauty of diversity in fashion
         . For those who don’t follow trends but set them. We’re here for the people who see fashion as limitless,
