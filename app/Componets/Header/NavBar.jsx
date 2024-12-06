84763835a392f1717e7a0d717e74d15b015c904b
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-05 19:40:48 -0500

    Update NavBar.jsx

--- app/Componets/Header/NavBar.jsx
+++ app/Componets/Header/NavBar.jsx
@@ -17,7 +17,7 @@ import Cart from './Cart'
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, DropdownItem, DropdownTrigger, Dropdown, DropdownMenu } from "@nextui-org/react";
 import { AiFillHome, AiOutlineShoppingCart } from 'react-icons/ai'
 import Link from 'next/link'
-import { IoMenu } from "react-icons/io5";
+import { CiMenuBurger } from "react-icons/ci";
 
 const alexFont = Bebas_Neue({
     weight: '400',
@@ -111,7 +111,7 @@ function NavBar() {
             <Navbar disableAnimation={true} className='bg-white' isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
                 <NavbarContent>
                     <NavbarMenuToggle
-                        icon={<IoMenu size={32} />}
+                        icon={<CiMenuBurger size={32} />}
                         aria-label={isMenuOpen ? "Close menu" : "Open menu"}
                         className="sm:hidden text-black"
                     />
