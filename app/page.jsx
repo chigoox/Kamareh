3c125f5cbd28747bb94b27b6fe3b3662fac73d8c
Emmanuel Dike <117424280+chigoox@users.noreply.github.com>
2024-12-20 01:01:44 -0500

    x remove

--- app/page.jsx
+++ app/page.jsx
@@ -15,11 +15,7 @@ function Home() {
   return (
     <div className='bg-white hidescroll  h-screen   relative overflow-x-hidden'>
       {/*       <MedicalCardMain /> */}
-      <EmblaCarousel
-        noArrow={true}
-        vid={'videos/MOV_3270.MOV'}
-
-      />
+     
 
 
       {/* <Image className=" border-white fadeIn  h-auto object-cover" src="/LogoW.png"></Image> */}
